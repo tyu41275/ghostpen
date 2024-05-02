8a8579f8e86c595867643c6724147ace8d35abb2
Alex Schwartz <schwartz@rdacorp.com>
2024-05-02 15:45:47 -0400

    Metadata/footer updates

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -16,7 +16,7 @@ const siteMetadata = {
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/yutim1275',
   threads: 'https://www.threads.net',
   instagram: 'https://www.instagram.com',
   locale: 'en-US',
