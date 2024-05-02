98e4f3016c98029db4970850642eec2c2918f6e9
Alex Schwartz <schwartz@rdacorp.com>
2024-05-02 15:46:40 -0400

    Navigation

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   // { href: '/blog', title: 'Blog' },
   // { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
+  { href: '/', title: 'Posts' },
   { href: '/about', title: 'About' },
 ]
 
