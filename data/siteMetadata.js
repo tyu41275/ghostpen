3ad6d9bf25632cae636f5fe3de2ef84e8144d638
Alex Schwartz <schwartz@rdacorp.com>
2024-05-02 15:42:41 -0400

    More cleanup

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Posts',
+  author: 'Tim Yu',
+  headerTitle: 'Posts',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
