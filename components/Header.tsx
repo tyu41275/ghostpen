62f5c77d2e0ad8571f31d195f1659bf2b5ef196c
Alex Schwartz <schwartz@rdacorp.com>
2024-05-02 15:43:36 -0400

    Remove search button

--- components/Header.tsx
+++ components/Header.tsx
@@ -37,7 +37,7 @@ const Header = () => {
               {link.title}
             </Link>
           ))}
-        <SearchButton />
+        {/* <SearchButton /> */}
         <ThemeSwitch />
         <MobileNav />
       </div>
